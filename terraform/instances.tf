f24fa6887b9ad96fd7a513c78313de2855a4bf32
avihu.hababo <avihulink@gmail.com>
2024-04-01 16:58:14 +0300

    shay final push

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example_server" {
   ami                    = "ami-010b74bc1a8b29122"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["${aws_security_group.terra.id}"]
-  key_name               = "github"
+  key_name               = "key_for_micros"
   subnet_id              = aws_subnet.public.id
 
   tags = {
